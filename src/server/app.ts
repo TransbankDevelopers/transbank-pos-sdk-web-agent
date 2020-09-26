6fb4e1e1d09f32b45707d53aa4eedb26bc344eab
Gonzalo De Spirito <gonzunigad@gmail.com>
2020-09-26 04:25:37 -0300

    Remove serve socket client

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http, {cookie: false});
+var io = require('socket.io')(http, {cookie: false, serveClient: false});
 import pos from '../pos';
 import windowManager from "../classes/window-manager"
 
