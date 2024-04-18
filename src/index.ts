3751a0e1e103e1066bacbd3e459e92b605fa87cb
Matias Vasquez <m.vasquezac@gmail.com>
2024-04-18 11:27:22 -0400

    feat: manage missing crt notice

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 const AutoLaunch = require('auto-launch');
-import { app, BrowserWindow, ipcMain, Tray, Menu, nativeImage} from 'electron';
+import { app, BrowserWindow, ipcMain, Tray, Menu, nativeImage, dialog, shell} from 'electron';
 import path from 'path'
 import pos from './pos'
 import windowManager from "./classes/window-manager"
+import {checkCertExists, getCrtPath} from './server/utils/utils'
+
 ipcMain.handle('connect-to-pos', (event, ...args) => {
   pos.autoconnect().then((port) => {
     if (port) {
@@ -90,10 +92,32 @@ const createWindow = (): BrowserWindow => {
 
 };
 
+const handleStart = () => {
+
+  if ( checkCertExists() ) {
+    return createWindow();
+  }
+  const opentPathOption = 0;
+  const selectedOption = dialog.showMessageBoxSync({
+    type: 'error',
+    title: 'Error de certificados',
+    message: `No se han encontrado los archivos de certificados. Debes copiarlos a la carpeta de destino e iniciar el agente nuevamente`,
+    buttons: ['Abrir carpeta', 'Cerrar agente']
+  });
+
+  if ( selectedOption == opentPathOption ) {   
+      shell.openPath(getCrtPath());
+  }
+
+  app.quit();
+
+};
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', handleStart);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
